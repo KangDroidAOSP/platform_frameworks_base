31ce1caee0d4d184f19d82e2f53555b3fa6f4480
rogersb11 <brettrogers11@gmail.com>
2015-05-08 02:06:29 +0200

    PIE 3.0 (1/2)
    Ported to lollipop and modified by Brett Rogers(CyanideL) & BlackDragon(Fusion).
    
    PIE Key contributors:
    BigBrother1984
    D4rKn3sSyS
    Anu6is
    David96
    drcmda
    
    P19:
       - Renamed pie directories
       - Renamed some pie class names
       - Modify copyright information
       - Use PieConstants into PieController class
    
    P20:
      fix BaseStatusBar class in reflection of P19 changes
    
    P21:
        - fix piehelper class in reference to P19
    
    P22:
       - fix piecontroller reference to P19
    
    P24:
      - fix crashes in pie control xml
    
    Note: Made to work with Marshmallow by xanaxdroid
    
    Signed-off-by: rogersb11 <brettrogesr11@gmail.com>
    Change-Id: I1838a300544bd5c769d82f1193065731baebdb48
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1848,12 +1848,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                     | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                     | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH
+                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
                 PixelFormat.TRANSLUCENT);
         // this will allow the navbar to run in an overlay on devices that support this
-        if (ActivityManager.isHighEndGfx()) {
+/*        if (ActivityManager.isHighEndGfx()) {
             lp.flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
-        }
+        }*/ //*Keep for possible future use.
 
         lp.setTitle("NavigationBar");
         lp.windowAnimations = 0;
@@ -3201,6 +3202,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null) {
             mNavigationBarView.setNavigationIconHints(hints);
         }
+
+        if (mPieController != null) {
+            mPieController.setNavigationIconHints(hints);
+        }
         checkBarModes();
     }
 
@@ -3488,6 +3493,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     public void topAppWindowChanged(boolean showMenu) {
+        if (mPieController != null && mPieController.getControlPanel() != null)
+            mPieController.getControlPanel().setMenu(showMenu);
+
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
         }
@@ -3795,6 +3803,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 notifyHeadsUpScreenOff();
                 finishBarAnimations();
                 resetUserExpandedStates();
+                // detach PA Pie when screen is turned off
+                if (mPieController != null) mPieController.detachPie();
             }
             else if (Intent.ACTION_CONFIGURATION_CHANGED.equals(action)) {
                 Configuration config = mContext.getResources().getConfiguration();
