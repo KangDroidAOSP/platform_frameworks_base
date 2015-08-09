9f81261cd3c3222b11eaf2dbcd652b2c6f32886e
Randall Rushing <randall.rushing@gmail.com>
2015-08-09 16:53:17 -0700

    Task Manager: Use one observer for changes, misc cleanup
    -- We use just one observer and propagate values to interested classes with setter methods
    
    -- Remove some null checks. We create the TaskManagerPanel regardless now.
    
    -- Explicitly set proper panel visibility state when feature becomes disabled
       while the TaskManager panel was visible
    
    -- Explicitly set TaskManager button click state to false when disabled
    
    -- Clean up StatusBarHeaderView SystemIcon LayoutParams for TeamEos implementation
    
    -- Survive a theme change
    
    Change-Id: I0f772e4743e4fa357f13f4ae705bc136ac9dffa3
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -385,8 +385,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private TaskManager mTaskManager;
     private LinearLayout mTaskManagerPanel;
     private ImageButton mTaskManagerButton;
-	private boolean mShowTaskManager;
-    private boolean showTaskList = false;
+    // task manager enabled
+    private boolean mShowTaskManager;
+    // task manager click state
+    private boolean mShowTaskList = false;
 
     // top bar
     StatusBarHeaderView mHeader;
@@ -688,8 +690,21 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mBlurRadius = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_BLUR_RADIUS, 14);
 			
-            mShowTaskManager = Settings.System.getIntForUser(resolver,
+	boolean showTaskManager = Settings.System.getIntForUser(resolver,
                     Settings.System.ENABLE_TASK_MANAGER, 0, UserHandle.USER_CURRENT) == 1;
+	    if (mShowTaskManager != showTaskManager) {
+                if (!mShowTaskManager) {
+                    // explicitly reset click state when disabled
+                    mShowTaskList = false;
+                }
+                mShowTaskManager = showTaskManager;
+                if (mHeader != null) {
+                    mHeader.setTaskManagerEnabled(showTaskManager);
+                }
+                if (mNotificationPanel != null) {
+                    mNotificationPanel.setTaskManagerEnabled(showTaskManager);
+                }
+            }
         }
     }
 
@@ -1520,10 +1535,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mTaskManagerButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                showTaskList = !showTaskList;
-                mNotificationPanel.setTaskManagerVisibility(showTaskList);
+                mShowTaskList = !mShowTaskList;
+                mNotificationPanel.setTaskManagerVisibility(mShowTaskList);
             }
         });
+		
+        if (mRecreating) {
+            mHeader.setTaskManagerEnabled(mShowTaskManager);
+            mNotificationPanel.setTaskManagerEnabled(mShowTaskManager);
+            mShowTaskList = false;
+        }
 
         // User info. Trigger first load.
         mHeader.setUserInfoController(mUserInfoController);
