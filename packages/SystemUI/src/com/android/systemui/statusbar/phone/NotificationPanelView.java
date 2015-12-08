4059e979e8af3e145c7d4fd91b4192dc4350ec15
Alex Cruz <mazdarider23@gmail.com>
2015-12-08 22:27:55 +0530

    SystemUI:Add Missing Parts for CAF Task Manager
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1722,6 +1722,14 @@ public class NotificationPanelView extends PanelView implements
         }
     }
 
+
+
+  private void cancelAnimation() {
+        if (mQsExpansionAnimator != null) {
+            mQsExpansionAnimator.cancel();
+        }
+    }
+
     @Override
     protected boolean isScrolledToBottom() {
         if (!isInSettings()) {
