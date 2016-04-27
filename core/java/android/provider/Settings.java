35d487ac16fff3ee314fc81fbda5c343ce827346
Varun Date <date.varun123@gmail.com>
2016-04-27 19:53:18 -0400

    Dyanamically Configurable Pulse Bar Dimensions [1/3]
    **DU Edits**
    
    - Move to Secure to avoid apps from messing with things as per Randall
    
    Change-Id: Iebe6ddab57e88006713eca3e01d2d98020207cc8
    
     FWB: Configurable Pulse Bar Spacing [1/3]
    
    Change-Id: I106e7a78e67e15ee55450b2e60f0e81836213cb5
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    Add lost settings for advanced pulse
    
    Change-Id: Ia3af63d8b4ddfcf33e29f7026999e655949c7c36

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7934,6 +7934,36 @@ public final class Settings {
          * @hide
          */
         public static final String FLING_LONGSWIPE_THRESHOLD_DOWN_LAND = "fling_longswipe_threshold_down_land";
+		
+        /**
+         * Custom Pulse Widths
+         * @hide
+         */
+        public static final String PULSE_CUSTOM_DIMEN = "pulse_custom_dimen";
+
+        /**
+         * Custom Spacing Between Pulse Bars
+         * @hide
+         */
+        public static final String PULSE_CUSTOM_DIV = "pulse_custom_div";
+
+        /**
+         * Custom Pulse Block Size
+         * @hide
+         */
+        public static final String PULSE_FILLED_BLOCK_SIZE = "pulse_filled_block_size";
+
+        /**
+         * Custom Spacing Between Pulse Blocks
+         * @hide
+         */
+        public static final String PULSE_EMPTY_BLOCK_SIZE = "pulse_empty_block_size";
+
+        /**
+         * Custom Pulse Sanity Levels
+         * @hide
+         */
+        public static final String PULSE_CUSTOM_FUDGE_FACTOR = "pulse_custom_fudge_factor";
 
         /**
          * This are the settings to be backed up.
