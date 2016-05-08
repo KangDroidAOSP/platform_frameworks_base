CyanogenMod/android_frameworks_base

Add new options for call waiting response in InCallUI
When an incoming call is received while there is already an active call,
the user will be given four choices for the response:

1) Left button: hold the current call and answer incoming call
2) Bottom button: end the current call and answer the incoming call
3) Right button: decline the incoming call
4) Top button: Send a text message to the incoming caller if texting
   is enabled

The framework changes include new hidden API for sending additional
information to Telecomm service, few constants, and a bug fix.

Change-Id: I642b330d856613fafc0526eb79dd97fedba4ce6a

telecomm/java/android/telecom/InCallAdapter.java
@@ -57,6 +57,20 @@ public final class InCallAdapter {
     }
 
     /**
+     * Instructs Telecom to answer the specified call.
+     *
+     * @param callId The identifier of the call to answer.
+     * @param videoState The video state in which to answer the call.
+     * @param callWaitingResponseType Response type for call waiting.
+     */
+    public void answerCall(String callId, int videoState, int callWaitingResponseType) {
+        try {
+            mAdapter.answerCallRevised(callId, videoState, callWaitingResponseType);
+        } catch (RemoteException e) {
+        }
+    }
+
+    /**
      * Instructs Telecom to reject the specified call.
      *
      * @param callId The identifier of the call to reject.

KangDroidAOSP/platform_frameworks_base

telecomm/java/android/telecom/InCallAdapter.java
@@ -57,6 +57,20 @@ public final class InCallAdapter {
     }
 
     /**
+     * Instructs Telecom to answer the specified call.
+     *
+     * @param callId The identifier of the call to answer.
+     * @param videoState The video state in which to answer the call.
+     * @param callWaitingResponseType Response type for call waiting.
+     */
+    public void answerCall(String callId, int videoState, int callWaitingResponseType) {
+        try {
+            mAdapter.answerCallWithCallWaitingResponse(callId, videoState, callWaitingResponseType);
+        } catch (RemoteException e) {
+        }
+    }
+
+    /**
      * Instructs Telecom to reject the specified call.
      *
      * @param callId The identifier of the call to reject.