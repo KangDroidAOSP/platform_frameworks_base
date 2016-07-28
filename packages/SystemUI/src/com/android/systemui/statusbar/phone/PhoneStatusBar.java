69d2796484b96d0edeb1f56f70b1be1eb3500d1a
KangDroid <hyunwoo286@gmail.com>
2016-07-28 22:02:39 +0900

    Remove some problemetic settings:
    - Remove QS Header/Panel Transparency Settings
    - Remove Time Contextual Header Settings
    - Remove SlimDim(It is not working)
    Change-Id: I7ac69125859131922d19788a3739bd8ea8b66816

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -150,7 +150,6 @@ import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.doze.DozeHost;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.keyguard.KeyguardViewMediator;
-import com.android.systemui.omni.StatusBarHeaderMachine;
 import com.android.systemui.qs.QSDragPanel;
 import com.android.systemui.qs.QSDetailItems;
 import com.android.systemui.qs.QSPanel;
@@ -966,7 +965,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mAutohideSuspended;
     private int mStatusBarMode;
     private int mNavigationBarMode;
-    private StatusBarHeaderMachine mStatusBarHeaderMachine;
 
     private ViewMediatorCallback mKeyguardViewMediatorCallback;
     private ScrimController mScrimController;
@@ -1802,20 +1800,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
-
-        mStatusBarHeaderMachine = new StatusBarHeaderMachine(mContext);
-        mStatusBarHeaderMachine.addObserver(mHeader);
-        mStatusBarHeaderMachine.updateEnablement();
         UpdateNotifDrawerClearAllIconColor();
         updateNetworkIconColors();
         return mStatusBarView;
     }
-	
-	public void updateheaderForKDP() {
-        mStatusBarHeaderMachine = new StatusBarHeaderMachine(mContext);
-        mStatusBarHeaderMachine.addObserver(mHeader);
-        mStatusBarHeaderMachine.updateEnablement();
-	}
 	
 	public void updateKDPWeatherContents() {
         mWeatherTempStyle = Settings.System.getIntForUser(
@@ -2895,7 +2883,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	mSignalView.setIconColor();
 	mTileView.setIconColor();
 	mTileView.updateColors();
-	mStatusBarHeaderMachine.doUpdateStatusHeaderObservers(true);
 	}
 
     private void updateStatusIconsColor() {
